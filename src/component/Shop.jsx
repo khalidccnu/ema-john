8833fdbc7d37068ae79526da388973af9977e3b3
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-03-27 19:57:50 +0600

    show all functionality added

--- src/component/Shop.jsx
+++ src/component/Shop.jsx
@@ -3,6 +3,8 @@ import Product from "./Product.jsx";
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [showAll, setShowAll] = useState(false);
+
     useEffect(_ => {
         fetch(`./products.json`).then(r => r.json()).then(data => setProducts(data));
     }, []);
@@ -10,8 +12,17 @@ const Shop = () => {
     return (
         <section className="py-5">
             <div className="container">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                    {products.map(product => <Product key={product.id} {...product} />)}
+                <div className="products">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {products.slice(0, showAll ? products.length : 6).map(product => <Product key={product.id} {...product} />)}
+                    </div>
+                    {
+                        showAll || (
+                            <div className="mt-5 text-center">
+                                <button type="button" className="btn btn-sm btn-info" onClick={_ => setShowAll(true)}>Show All</button>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </section>
