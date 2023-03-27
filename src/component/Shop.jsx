09634fb2b0ac6f4ff68b5e79260de8b41b6f7d88
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-03-28 02:31:50 +0600

    spinner added

--- src/component/Shop.jsx
+++ src/component/Shop.jsx
@@ -1,17 +1,24 @@
 import React, {useEffect, useState} from 'react';
+import {CircleLoader} from "react-spinners";
 import Products from "./Products.jsx";
 import Cart from "./Cart.jsx";
 import 'boxicons';
 import {getCartFromLS} from "../utility/index.js";
 
 const Shop = () => {
+    const [loading, setLoading] = useState(true);
     const [products, setProducts] = useState([]);
     const [showAll, setShowAll] = useState(false);
     const [cart, setCart] = useState([]);
     const [addToCart, setAddToCart] = useState(false);
 
     useEffect(_ => {
-        fetch(`./products.json`).then(r => r.json()).then(data => setProducts(data));
+        (async _ => {
+            !loading ? await setLoading(true) : null;
+            await fetch(`./products.json`).then(r => r.json()).then(data => setProducts(data));
+
+            setLoading(false);
+        })();
     }, []);
 
     useEffect(_ => {
@@ -34,8 +41,18 @@ const Shop = () => {
     return (
         <section className="py-5">
             <div className="container relative">
-                <Products products={products} addToCart={setAddToCart} showAll={showAll} setShowAll={setShowAll} />
-                <Cart cart={cart} addToCart={setAddToCart} />
+                {
+                    loading ? (
+                        <div className="mx-auto w-fit">
+                            <CircleLoader color="#36d7b7" />
+                        </div>
+                    ) : (
+                        <>
+                        <Products products={products} addToCart={setAddToCart} showAll={showAll} setShowAll={setShowAll} />
+                        <Cart cart={cart} addToCart={setAddToCart} />
+                        </>
+                    )
+                }
             </div>
         </section>
     );
