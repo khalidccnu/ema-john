8cac41ae814bca9036f2d805035f7476dc55f35c
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-03-29 22:00:23 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./component/Nav.jsx";
 import Shop from "./component/Shop.jsx";
+import Footer from "./component/Footer.jsx";
 
 function App() {
   return (
       <>
       <Nav />
       <Shop />
+      <Footer />
       </>
   )
 }
