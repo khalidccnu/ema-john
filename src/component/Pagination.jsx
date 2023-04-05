abf72094d2e020f6aa313c31463ae56ce459219d
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-04-05 20:31:09 +0600

    pagination fixed

--- src/component/Pagination.jsx
+++ src/component/Pagination.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 
 const Pagination = ({ total, currentPage, setCurrentPage, perPage }) => {
+  let pageCount = Math.ceil(total / perPage);
   const pages = [];
 
-  for (let i = 1; i <= Math.ceil(total / perPage); i++) pages.push(i);
+  for (let i = 1; i <= pageCount; i++) pages.push(i);
 
   return (
     <div className="flex justify-center mt-5">
       <div className="btn-group">
         {pages.map((page, idx) => {
-          return (
-            <button
-              key={idx}
-              className={`btn btn-sm ${
-                currentPage === page
-                  ? "btn-active btn-disabled !bg-accent"
-                  : null
-              } border-purple-500`}
-              onClick={(_) => setCurrentPage(page)}
-            >
-              {page}
-            </button>
-          );
+          if (
+            page === currentPage ||
+            (page < currentPage && page >= currentPage - 2) ||
+            (page > currentPage && page <= currentPage + 2)
+          ) {
+            return (
+              <button
+                key={idx}
+                className={`btn btn-sm ${
+                  currentPage === page
+                    ? "btn-active btn-disabled !bg-accent"
+                    : null
+                } border-purple-500`}
+                onClick={(_) => setCurrentPage(page)}
+              >
+                {page}
+              </button>
+            );
+          } else {
+            return null;
+          }
         })}
       </div>
     </div>
